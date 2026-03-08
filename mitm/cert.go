54a378385b6d90fd804767083d70530b52419abb
naiba <hi@nai.ba>
2026-03-08 03:59:49 +0000

    refactor: replace getlantern/keyman with stdlib crypto for MITM CA
    keyman has 16 stars and hasn't been updated since 2023.
    All its functionality (RSA key generation, X.509 cert creation,
    PEM encoding) is trivially available in Go stdlib crypto packages.
    
    Co-authored-by: naiba/CloudCode <hi+cloudcode@nai.ba>

--- mitm/cert.go
+++ mitm/cert.go
@@ -1,11 +1,16 @@
 package mitm
 
 import (
+	"crypto/rand"
+	"crypto/rsa"
 	"crypto/tls"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
 	"fmt"
+	"math/big"
+	"os"
 	"time"
-
-	"github.com/getlantern/keyman"
 )
 
 const (
@@ -15,20 +20,21 @@ const (
 	year        = 1
 )
 
-//GenerateCA rt
+// GenerateCA rt
 func (m *MITM) GenerateCA() error {
 	var err error
-	if m.pk, err = keyman.LoadPKFromFile(m.TLSConf.PrivateKeyFile); err != nil {
-		m.pk, err = keyman.GeneratePK(2048)
+	if m.pk, err = loadPKFromFile(m.TLSConf.PrivateKeyFile); err != nil {
+		m.pk, err = rsa.GenerateKey(rand.Reader, 2048)
 		if err != nil {
 			return fmt.Errorf("Unable to generate private key: %s", err)
 		}
-		m.pk.WriteToFile(m.TLSConf.PrivateKeyFile)
+		writePKToFile(m.pk, m.TLSConf.PrivateKeyFile)
 	}
-	m.pkPem = m.pk.PEMEncoded()
-	m.issuingCert, err = keyman.LoadCertificateFromFile(m.TLSConf.CertFile)
-	if err != nil || m.issuingCert.ExpiresBefore(time.Now().AddDate(0, month, 0)) {
-		m.issuingCert, err = m.pk.TLSCertificateFor(
+	m.pkPem = pemEncodePrivateKey(m.pk)
+	m.issuingCert, err = loadCertificateFromFile(m.TLSConf.CertFile)
+	if err != nil || m.issuingCert.NotAfter.Before(time.Now().AddDate(0, month, 0)) {
+		m.issuingCert, err = createCertificate(
+			m.pk,
 			time.Now().AddDate(year, 0, 0),
 			true,
 			nil,
@@ -38,12 +44,12 @@ func (m *MITM) GenerateCA() error {
 		if err != nil {
 			return fmt.Errorf("Unable to generate self-signed issuing certificate: %s", err)
 		}
-		m.issuingCert.WriteToFile(m.TLSConf.CertFile)
+		writeCertToFile(m.issuingCert, m.TLSConf.CertFile)
 	}
 	return nil
 }
 
-//FakeCert rt
+// FakeCert rt
 func (m *MITM) FakeCert(domain string) (*tls.Certificate, error) {
 	cert, has := m.cache.Get("DC" + domain)
 	if has {
@@ -52,7 +58,8 @@ func (m *MITM) FakeCert(domain string) (*tls.Certificate, error) {
 
 	//create certificate
 	certTTL := doubleWeeks
-	generatedCert, err := m.pk.TLSCertificateFor(
+	generatedCert, err := createCertificate(
+		m.pk,
 		time.Now().Add(certTTL),
 		false,
 		m.issuingCert,
@@ -61,11 +68,119 @@ func (m *MITM) FakeCert(domain string) (*tls.Certificate, error) {
 	if err != nil {
 		return nil, fmt.Errorf("Unable to issue certificate: %s", err)
 	}
-	keyPair, err := tls.X509KeyPair(generatedCert.PEMEncoded(), m.pkPem)
+	keyPair, err := tls.X509KeyPair(pemEncodeCertificate(generatedCert), m.pkPem)
 	if err != nil {
 		return nil, fmt.Errorf("Unable to parse keypair for tls: %s", err)
 	}
 
 	m.cache.Set("DC"+domain, &keyPair, time.Hour*48)
 	return &keyPair, nil
+}
+
+// loadPKFromFile 从PEM文件加载RSA私钥
+func loadPKFromFile(path string) (*rsa.PrivateKey, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	block, _ := pem.Decode(data)
+	if block == nil {
+		return nil, fmt.Errorf("failed to decode PEM block from %s", path)
+	}
+	return x509.ParsePKCS1PrivateKey(block.Bytes)
+}
+
+// writePKToFile 将RSA私钥以PEM格式写入文件
+func writePKToFile(pk *rsa.PrivateKey, path string) error {
+	f, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	return pem.Encode(f, &pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(pk),
+	})
+}
+
+// pemEncodePrivateKey 返回RSA私钥的PEM编码字节
+func pemEncodePrivateKey(pk *rsa.PrivateKey) []byte {
+	return pem.EncodeToMemory(&pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(pk),
+	})
+}
+
+// loadCertificateFromFile 从PEM文件加载X.509证书
+func loadCertificateFromFile(path string) (*x509.Certificate, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	block, _ := pem.Decode(data)
+	if block == nil {
+		return nil, fmt.Errorf("failed to decode PEM block from %s", path)
+	}
+	return x509.ParseCertificate(block.Bytes)
+}
+
+// writeCertToFile 将X.509证书以PEM格式写入文件
+func writeCertToFile(cert *x509.Certificate, path string) error {
+	f, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	return pem.Encode(f, &pem.Block{
+		Type:  "CERTIFICATE",
+		Bytes: cert.Raw,
+	})
+}
+
+// pemEncodeCertificate 返回X.509证书的PEM编码字节
+func pemEncodeCertificate(cert *x509.Certificate) []byte {
+	return pem.EncodeToMemory(&pem.Block{
+		Type:  "CERTIFICATE",
+		Bytes: cert.Raw,
+	})
+}
+
+// createCertificate 创建X.509证书（CA自签名或由issuer签发的域名证书）
+func createCertificate(pk *rsa.PrivateKey, expiry time.Time, isCA bool, issuer *x509.Certificate, org string, cn string) (*x509.Certificate, error) {
+	serialNumber, err := rand.Int(rand.Reader, new(big.Int).Lsh(big.NewInt(1), 128))
+	if err != nil {
+		return nil, fmt.Errorf("failed to generate serial number: %s", err)
+	}
+
+	template := &x509.Certificate{
+		SerialNumber: serialNumber,
+		Subject: pkix.Name{
+			Organization: []string{org},
+			CommonName:   cn,
+		},
+		NotBefore: time.Now(),
+		NotAfter:  expiry,
+	}
+
+	if isCA {
+		template.BasicConstraintsValid = true
+		template.IsCA = true
+		template.KeyUsage = x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign
+	} else {
+		template.DNSNames = []string{cn}
+		template.KeyUsage = x509.KeyUsageDigitalSignature
+	}
+
+	// 自签名时 parent 为自身，否则使用 issuer 作为 parent
+	parent := template
+	if issuer != nil {
+		parent = issuer
+	}
+
+	certDER, err := x509.CreateCertificate(rand.Reader, template, parent, &pk.PublicKey, pk)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create certificate: %s", err)
+	}
+
+	return x509.ParseCertificate(certDER)
 }
